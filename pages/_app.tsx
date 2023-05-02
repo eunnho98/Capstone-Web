4afa3a801dca2714fff7a37a7ac52e3ee42029c5
eunnho <taemin4u@khu.ac.kr>
2023-05-02 11:36:26 +0900

    dev: naver map 띄우기

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,6 +3,7 @@ import { AppProps } from 'next/app';
 import { RecoilRoot } from 'recoil';
 import { QueryClientProvider, QueryClient } from 'react-query';
 import { ReactQueryDevtools } from 'react-query/devtools';
+import Script from 'next/script';
 const queryClient = new QueryClient();
 
 function MyApp({ Component, pageProps }: AppProps) {
@@ -10,6 +11,10 @@ function MyApp({ Component, pageProps }: AppProps) {
     <ChakraProvider>
       <QueryClientProvider client={queryClient}>
         <RecoilRoot>
+          <Script
+            strategy="beforeInteractive"
+            src={`https://openapi.map.naver.com/openapi/v3/maps.js?ncpClientId=${process.env.NEXT_PUBLIC_NAVER_MAP_CLIENTID}`}
+          />
           <Component {...pageProps} />
           <ReactQueryDevtools />
         </RecoilRoot>
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,27 +1,35 @@
-import { Button } from '@chakra-ui/react';
-import { getAllBusNumberAPI } from '@/lib/api/bus';
-import { IGetAllBusNumber } from '@/lib/type';
+import { Box, Button } from '@chakra-ui/react';
+import { getBusInfoAPI } from '@/lib/api/bus';
 import { useQuery } from 'react-query';
+import useMap from '@/lib/hooks/useMap';
+
+// export default function Home() {
+//   const { data, refetch, isLoading } = useQuery(
+//     'allBus',
+//     // 파라미터를 주기 위해서
+//     () => getBusInfoAPI({ cityCode: 25, routeId: 'DJB30300004' }),
+//   );
+
+//   const handleBtn = () => {
+//     refetch();
+//   };
+//   if (isLoading) {
+//     return <h2>loading...</h2>;
+//   }
+//   return (
+//     <div>
+//       <Button onClick={handleBtn}>sdf</Button>
+//       <div>{JSON.stringify(data)}</div>
+//     </div>
+//   );
+// }
 
 export default function Home() {
-  const { data, refetch, isLoading } = useQuery(
-    'allBus',
-    // 파라미터를 주기 위해서
-    () => getAllBusNumberAPI({ cityCode: 26, pageNo: 20 }),
-  );
-
-  const handleBtn = () => {
-    refetch();
-  };
-  if (isLoading) {
-    return <h2>loading...</h2>;
-  }
+  useMap();
   return (
     <div>
-      <Button onClick={handleBtn}>sdf</Button>;
-      {data.map((bus) => (
-        <li key={bus.routeId}>{bus.startnodenm}</li>
-      ))}
+      <Box id="map" w="800px" h="600px"></Box>
+      <p>sssdf</p>
     </div>
   );
 }
