ceee1807f36542f6dc13f11430d9ea1f71056483
eunnho <taemin4u@khu.ac.kr>
2023-06-01 18:38:42 +0900

    test

--- pages/auth.tsx
+++ pages/auth.tsx
@@ -112,7 +112,7 @@ function login() {
 
   const onTest = async () => {
     const googleLoginURL =
-      'https://accounts.google.com/o/oauth2/v2/auth?scope=openid https://www.googleapis.com/auth/userinfo.email https://www.googleapis.com/auth/userinfo.profile&access_type=offline&include_granted_scope=true&response_type=code&state=state_parameter_passthrough_value&redirect_uri=https://yokhuroute.store/login/oauth2/code/google&client_id=759416534029-0idv1eac509hpu7h66na8bn4pug1k9ou.apps.googleusercontent.com';
+      'https://accounts.google.com/o/oauth2/v2/auth?scope=openid https://www.googleapis.com/auth/userinfo.email https://www.googleapis.com/auth/userinfo.profile&access_type=offline&include_granted_scope=true&response_type=code&state=state_parameter_passthrough_value&redirect_uri=https://capstone-web-zeta.vercel.app/api/auth/getCode&client_id=759416534029-0idv1eac509hpu7h66na8bn4pug1k9ou.apps.googleusercontent.com';
     router.push(googleLoginURL);
     const res = await getCodeAPI();
     console.log(res);
