69ac22f97a3f123ae6940faf7551ba3401f97e68
eunnho <taemin4u@khu.ac.kr>
2023-06-01 19:28:17 +0900

    sdfsffa

--- lib/api/auth.ts
+++ lib/api/auth.ts
@@ -2,4 +2,5 @@ import axios from '.';
 
 export const loginAPI = () => axios.get('/login/oauth2/code/google');
 
-export const getCodeAPI = () => axios.get('/api/auth/getCode');
+export const getCodeAPI = () =>
+  axios.get('https://capstone-web-zeta.vercel.app/api/auth/getCode');
