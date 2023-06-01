2e7c280d926a14aa9fffe87521171bed9f83a514
eunnho <taemin4u@khu.ac.kr>
2023-06-01 18:48:03 +0900

    test

--- pages/api/auth/getCode.ts
+++ pages/api/auth/getCode.ts
@@ -5,6 +5,7 @@ export default function (req: NextApiRequest, res: NextApiResponse) {
   if (req.method === 'GET') {
     console.log(req);
     const { code } = req.query;
+    console.log('code:', code);
     res.json(code);
   }
 }
